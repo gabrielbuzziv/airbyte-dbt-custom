f1e07c1ab16473ce4a49c281b1130dd87b58cb0b
Gabriel Buzzi Venturi <gabrielbuzziv@gmail.com>
2022-11-21 15:57:49 -0300

    add teste

---
+++ models/generated/airbyte_tables/public/teste.sql
@@ -0,0 +1,18 @@
+{{ config(
+    indexes = [{'columns':['_airbyte_emitted_at'],'type':'btree'}],
+    unique_key = '_airbyte_ab_id',
+    schema = "public",
+    tags = [ "top-level" ]
+) }}
+-- Final base SQL model
+-- depends_on: {{ ref('users_ab3') }}
+select
+    {{ adapter.quote('id') }},
+    {{ adapter.quote('name') }},
+    slug,
+    {{ current_timestamp() }} as _airbyte_normalized_at,
+    _airbyte_users_hashid
+from {{ ref('users_ab3') }}
+-- users from {{ source('public', '_airbyte_raw_users') }}
+where 1 = 1
+
