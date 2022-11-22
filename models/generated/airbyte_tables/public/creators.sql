dc773916c7685ae32a0c3645509f6222aee1dce3
Gabriel Buzzi Venturi <gabrielbuzziv@gmail.com>
2022-11-22 17:04:08 -0300

    new try

--- models/generated/airbyte_tables/public/creators.sql
+++ models/generated/airbyte_tables/public/creators.sql
@@ -6,15 +6,37 @@
 ) }}
 -- Final base SQL model
 -- depends_on: {{ ref('airbyte_users_ab3') }}
-select
-    {{ ref('airbyte_users_ab3') }}.{{ adapter.quote('id') }},
-    {{ ref('airbyte_users_ab3') }}.{{ adapter.quote('name') }},
+
+
+WITH users AS (
+    SELECT * FROM {{ ref('airbyte_users_ab3') }}
+), plan_subscriptions AS (
+    SELECT * FROM {{ ref('airbyte_plan_subscriptions_ab3') }}
+), plans AS (
+    SELECT * FROM {{ ref('airbyte_plans_ab3') }}
+)
+
+SELECT
+    u.id,
+    u.id as atlas_user_id,
+    u.name,
+    u.slug,
+    concat('https://xesque.rocketseat.dev/users/avatar/', u.avatar) as avatar_url,
+    u.company_name as company,
+    u.occupation,
+    u.about,
+    u.created_at,
+    u.updated_at
     _airbyte_ab_id,
     _airbyte_emitted_at,
     {{ current_timestamp() }} as _airbyte_normalized_at,
-    _airbyte_airbyte_users_hashid
-from {{ ref('airbyte_users_ab3') }}
-inner join {{ ref('airbyte_plan_subscriptions_ab3') }} on {{ ref('airbyte_plan_subscriptions_ab3') }}.user_id = {{ ref('airbyte_users_ab3') }}.id
--- airbyte_users from {{ source('public', '_airbyte_raw_airbyte_users') }}
-where 1 = 1
-
+    _airbyte_airbyte_creators_hashid
+FROM plan_subscriptions ps
+    inner join plans p on p.id = ps.plan_id
+    inner join users u on ps.user_id = u.id
+WHERE
+    p.type = 'expertsclub'
+    AND plan_id = '77053ad2-b904-48bc-921f-decc7ee82638'
+    AND ps.canceled is false
+    AND ps.active is true
+    AND ps.ends_at > now();
