7381d6c8d997e2a4d370953ba50f80655b371eac
Gabriel Buzzi Venturi <gabrielbuzziv@gmail.com>
2022-11-22 16:46:09 -0300

    improve creators

--- models/generated/airbyte_tables/public/creators.sql
+++ models/generated/airbyte_tables/public/creators.sql
@@ -7,13 +7,22 @@
 -- Final base SQL model
 -- depends_on: {{ ref('airbyte_users_ab3') }}
 select
-    {{ adapter.quote('id') }},
-    {{ adapter.quote('name') }},
-    _airbyte_ab_id,
-    _airbyte_emitted_at,
-    {{ current_timestamp() }} as _airbyte_normalized_at,
-    _airbyte_airbyte_users_hashid
-from {{ ref('airbyte_users_ab3') }}
--- airbyte_users from {{ source('public', '_airbyte_raw_airbyte_users') }}
-where 1 = 1
-
+    u.id,
+    u.id as atlas_user_id,
+    u.name,
+    u.slug,
+    concat('https://xesque.rocketseat.dev/users/avatar/', u.avatar) as avatar_url,
+    u.company_name as company,
+    u.occupation,
+    u.about,
+    u.created_at,
+    u.updated_at
+from {{ ref('airbyte_plan_subscriptions_ab3') }} ps
+    inner join {{ ref('airbyte_plans_ab3') }} p on p.id = ps.plan_id
+    inner join {{ ref('airbyte_users_ab3') }} u on ps.user_id = u.id
+where
+    p.type = 'expertsclub'
+    and plan_id = '77053ad2-b904-48bc-921f-decc7ee82638'
+    and ps.canceled is false
+    and ps.active is true
+    and ps.ends_at > now();
