feccc1971fe16d2e9bff1843ce5e3e36a0e52515
Gabriel Buzzi Venturi <gabrielbuzziv@gmail.com>
2022-11-22 17:50:08 -0300

    add data on plans alias

--- models/generated/airbyte_tables/public/teste.sql
+++ models/generated/airbyte_tables/public/teste.sql
@@ -12,13 +12,21 @@ with plans as (
     select
         {{ adapter.quote('id') }},
         {{ adapter.quote('type') }},
-        title
+        title,
+        _airbyte_ab_id,
+        _airbyte_emitted_at,
+        {{ current_timestamp() }} as _airbyte_normalized_at,
+        _airbyte_airbyte_plans_hashid
     from {{ ref('airbyte_plans_ab3') }}
 )
 
 select
     {{ adapter.quote('id') }},
-    {{ adapter.quote('type') }} as tipo_do_plano
+    {{ adapter.quote('type') }} as tipo_do_plano,
+    _airbyte_ab_id,
+    _airbyte_emitted_at,
+    {{ current_timestamp() }} as _airbyte_normalized_at,
+    _airbyte_airbyte_plans_hashid
 from plans
 where title = 'ExpertsClub - Creators'
 
