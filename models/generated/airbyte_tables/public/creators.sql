82263c8d6bbc182638e58c342241ab2d1d8d7044
Gabriel Buzzi Venturi <gabrielbuzziv@gmail.com>
2022-11-22 16:57:38 -0300

    try alias

--- models/generated/airbyte_tables/public/creators.sql
+++ models/generated/airbyte_tables/public/creators.sql
@@ -7,15 +7,14 @@
 -- Final base SQL model
 -- depends_on: {{ ref('airbyte_users_ab3') }}
 select
-    {{ adapter.quote('id') }},
-    {{ adapter.quote('name') }},
-    slug,
-    about,
+    {{ ref('airbyte_users_ab3') }}.{{ adapter.quote('id') }},
+    {{ ref('airbyte_users_ab3') }}.{{ adapter.quote('name') }},
     _airbyte_ab_id,
     _airbyte_emitted_at,
     {{ current_timestamp() }} as _airbyte_normalized_at,
     _airbyte_airbyte_users_hashid
 from {{ ref('airbyte_users_ab3') }}
+inner join {{ ref('airbyte_plan_subscriptions_ab3') }} on {{ ref('airbyte_plan_subscriptions_ab3') }}.user_id = {{ ref('airbyte_users_ab3') }}.id
 -- airbyte_users from {{ source('public', '_airbyte_raw_airbyte_users') }}
 where 1 = 1
 
