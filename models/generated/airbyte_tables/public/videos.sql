8e6511d677c7c697182258eaa1700b3757998f34
Gabriel Buzzi Venturi <gabrielbuzziv@gmail.com>
2022-11-23 10:02:33 -0300

    add videos table

---
+++ models/generated/airbyte_tables/public/videos.sql
@@ -0,0 +1,34 @@
+{{ config(
+    indexes = [{'columns':['_airbyte_emitted_at'],'type':'btree'}],
+    unique_key = '_airbyte_ab_id',
+    schema = "public",
+    tags = [ "top-level" ]
+) }}
+
+
+WITH lesson_history AS (
+  SELECT * FROM {{ ref('airbyte_users_ab3') }}
+)
+
+SELECT 
+    lh.id, 
+    lh.resource as jupiter_video_id, 
+    lh.author_id as creator_id, 
+    lh.title, 
+    lh.slug, 
+    lh.description, 
+    CONCAT('https://xesque.rocketseat.dev/outputs/', lh.resource, '/poster-25_0-experts.jpg') as thumbnail_url,
+    lh.is_free,
+    lh.unlisted as is_listed,
+    lh.is_searchable,
+    lh.release_at,
+    lh.created_at,
+    lh.updated_at,
+    gen_random_uuid() as _airbyte_ab_id,
+    {{ current_timestamp() }} as _airbyte_emitted_at,
+    {{ current_timestamp() }} as _airbyte_normalized_at,
+    gen_random_uuid() as _airbyte_videos_hashid
+FROM lesson_history lh
+WHERE
+    lh.default_experts_content = true
+    AND lh.is_expert_content = true
